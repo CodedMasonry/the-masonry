e15fc5f82267e0989ecbac84e36f10b26755ba28
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2024-11-21 20:48:49 -0500

    include file

---
+++ src/app/client-carousel.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { motion } from "motion/react";
+import { ImageCarousel } from "~/components/img-carousel";
+
+export default function ClientCarousel({ images }: { images: Array<string> }) {
+  // First Half of images
+  const setOne = images.slice(0, images.length / 2);
+  // Second half
+  const setTwo = images.slice(images.length / 2, images.length);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.25, type: "spring" }}
+      className="flex w-full flex-col"
+    >
+      <ImageCarousel images={setOne} direction="forward" />
+      <ImageCarousel images={setTwo} direction="backward" />
+    </motion.div>
+  );
+}
