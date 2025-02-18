2fedbb5145c7095a0433c5d144e28f6019221a5e
Mason <109301662+CodedMasonry@users.noreply.github.com>
2025-02-17 20:13:07 -0500

    move outline of carousel to image

--- src/components/img-carousel.tsx
+++ src/components/img-carousel.tsx
@@ -28,7 +28,7 @@ export default function ImageCarousel({
         {images.map((key, index) => (
           <CarouselItem
             key={index}
-            className="group relative ml-2 aspect-video h-36 w-64 basis-1/2 rounded-lg border-2 border-transparent transition hover:border-primary md:h-64 md:w-auto md:basis-1/4"
+            className="group relative ml-2 aspect-video h-36 w-64 basis-1/2 rounded-lg md:h-64 md:w-auto md:basis-1/4"
             onMouseEnter={() => plugin.current.stop()}
             onMouseLeave={() => plugin.current.play()}
           >
@@ -41,7 +41,7 @@ export default function ImageCarousel({
                 alt=""
                 fill
                 loading="lazy"
-                className="aspect-video rounded-lg"
+                className="aspect-video rounded-lg border-2 border-transparent transition-all group-hover:border-primary"
               />
             </Link>
             <IconExternalLink className="absolute bottom-2 right-2 z-20 translate-y-2 stroke-primary opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100" />
