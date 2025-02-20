9f67226ad46bfa9fa0684a1621c5ee23a06ab96c
Mason <109301662+CodedMasonry@users.noreply.github.com>
2025-02-20 11:35:08 -0500

    Add personal narrative

---
+++ src/app/personal-narrative/page.tsx
@@ -0,0 +1,68 @@
+import Footer from "~/components/footer";
+import { Navbar } from "~/components/navbar";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+// Dublin green: #006853
+export default async function Page() {
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+
+      <Header />
+
+      <Footer />
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <div className="ml-4 mr-4 mt-10 flex w-fit flex-col rounded-xl border-2 border-primary bg-muted p-4 md:ml-8">
+      <h1 className="text-5xl font-bold text-primary">Personal Narrative</h1>
+      <p className="mt-2 italic">Spring, 2025</p>
+      <p className="mt-4 max-w-6xl text-lg">
+        When I was younger my family didn’t want to pay for a babysitter, so
+        they handed me an iPad. I Spent hours a day on that iPad; it started my
+        journey into tech. In middle school my father gave me his old laptop,
+        and I proceeded to spend hours a day on it instead. From gaming to
+        writing code, it was a creative outlet. This laptop was the catalyst for
+        my interest in technology, and to the point now where I have significant
+        knowledge on nearly every aspect of it, and seen as a reliable source of
+        information regarding technology.
+      </p>
+      <p className="mt-4 max-w-6xl text-lg">
+        During my 8th grade year I received my first desktop computer. From
+        choosing the parts: the cpu, the gpu, to the case, to the monitors; it
+        was mine. Learning to build it expanded my knowledge greatly.
+        Eventually, I learned to create games by taking leaked games and
+        dissecting them, and trying to figure out how they worked. If something
+        didn’t make sense, I would research it. Seeing real, in production code
+        helped me understand the intricacies of coding, and improve my skills.
+        It’s how I learn to this day. A lecture on writing doesn’t teach someone
+        how to write, it’s repeated exposure to proper writing that does.
+      </p>
+      <p className="mt-4 max-w-6xl text-lg">
+        I stumbled upon the exploiting community in middle school. People who
+        would create code to gain an advantage in a game. What I learned early
+        on is people in markets like that don’t act in good faith–it only took a
+        few viruses to understand that. Going into high school, I shifted focus
+        to ethical hacking, and eventually cybersecurity. Taking a cybersecurity
+        academy reinforced that idea; however, knowing everything discussed in
+        that class made it quite boring.
+      </p>
+      <p className="mt-4 max-w-6xl text-lg">
+        I lost interest in programming for a year during my cybersecurity phase,
+        but taking Computer Science A and surrounding myself with like minded
+        people re-instilled that passion. Writing code taught me to have
+        patience, continuously iterate, and strive to keep learning. I recently
+        joined the robotics club to challenge myself further by working in a
+        team, across multiple different disciplines. Down the line, I will use
+        my technical skills and soft skills that I learn to eventually move to
+        leadership positions to help other passionate people succeed at their
+        goals.
+      </p>
+    </div>
+  );
+}
--- src/app/ypa/page.tsx
+++ src/app/ypa/page.tsx
@@ -2,13 +2,18 @@ import Footer from "~/components/footer";
 import { Navbar } from "~/components/navbar";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { IconArrowRight } from "@tabler/icons-react";
 
 // Dublin green: #006853
 export default async function Page() {
   return (
     <main className="flex flex-col">
       <Navbar />
+
       <Header />
+      <About />
+      <AboutNarrative />
 
       <Footer />
     </main>
@@ -18,15 +23,67 @@ export default async function Page() {
 function Header() {
   return (
     <div className="ml-4 mr-4 mt-16 flex flex-col md:ml-8">
+      <Image
+        src="/icons/dcs.png"
+        width={128}
+        height={112}
+        alt="Dublin City School's Logo"
+        className="aspect-auto"
+      />
       <h1 className="text-5xl font-bold text-[#006853]">
         Young Professionals Academy
       </h1>
-      <p className="mt-4 max-w-4xl text-lg">
+      <div className="mt-2 grid max-w-4xl grid-cols-6">
+        <p className="italic">Spring, 2025</p>
+        <p className="italic">Dublin City Schools</p>
+      </div>
+      <p className="mt-2 max-w-4xl text-lg">
         During my junior year in highschool I tried taking a program called IT
-        Academy. I decided halfway through the semester that I knew the contents
-        of the class, and decided to pick up Young Professionals Academy for my
-        second semester. It was one of the greatest decisions I have made.
+        Academy. I decided halfway through the semester to drop the class
+        because I knew the contents of the class, and decided to pick up Young
+        Professionals Academy for my second semester. It was one of the greatest
+        decisions I have made.
       </p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="ml-4 mr-4 mt-16 flex flex-col md:ml-8">
+      <h2 className="text-3xl font-bold underline decoration-[#006853]">
+        Who Am I?
+      </h2>
+      <p className="mt-2 max-w-4xl text-lg">
+        To give you a rough idea, I created a branding board.
+      </p>
+      <div className="relative aspect-video w-full">
+        <Image
+          src="https://hz2lv8281m.ufs.sh/f/v97wqiBL7HaNf3u7v0Pq0gtkDfMGlmTpwK42cP76vrUxEWzL"
+          alt="Branding Board covering details of who I am"
+          fill
+          className="aspect-video"
+        />
+      </div>
+    </div>
+  );
+}
+
+function AboutNarrative() {
+  return (
+    <div className="ml-4 mr-4 mt-16 flex flex-col md:ml-8">
+      <h2 className="text-3xl font-bold underline decoration-[#006853]">
+        If you want to know my history
+      </h2>
+      <p className="mt-2 max-w-4xl text-lg">
+        I wrote a short narrative about what lead to where I am today.
+      </p>
+      <Link href="/personal-narrative" className="mt-2">
+        <Button size="lg" className="bg-[#006853] hover:bg-[#008158]">
+          Read The Narrative
+          <IconArrowRight />
+        </Button>
+      </Link>
     </div>
   );
 }
