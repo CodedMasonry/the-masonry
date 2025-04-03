ece706d45bd9bb82f862b646592645da513ce2ef
Mason <109301662+CodedMasonry@users.noreply.github.com>
2025-04-03 11:20:55 -0400

    add IA

---
+++ src/app/(public)/(temp)/risk-at-rest/page.tsx
@@ -0,0 +1,12 @@
+import { IconDownload } from "@tabler/icons-react";
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <>
+      <div className="relative h-svh w-full md:mt-16">
+        <iframe src="/risk-at-rest.pdf" className="h-svh w-full" />
+      </div>
+    </>
+  );
+}
