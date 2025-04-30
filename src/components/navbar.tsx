db9c0dda9a1126a1d1a298e90ad4ca3cae2391a4
Mason <109301662+CodedMasonry@users.noreply.github.com>
2025-04-30 17:52:41 -0400

    Test if removing sidebar improves mobile performance

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,15 @@
 "use client";
 
-import { useIsMobile } from "~/hooks/use-mobile";
 import { ModeToggle } from "./mode-toggle";
 import { NavbarMenu } from "./navbarRouting";
-import { useSidebar } from "./ui/sidebar";
-import { Button } from "./ui/button";
-import { type Icon, IconMenu2 } from "@tabler/icons-react";
 import React from "react";
 import { ProfessionalSection } from "~/lib/routing";
 
 export function Navbar() {
-  const isMobile = useIsMobile();
-  const { toggleSidebar } = useSidebar();
-
   return (
     <div className="absolute flex w-full items-center p-2">
       <div className="ml-4">
-        {isMobile ? (
-          <Button variant="ghost" size="icon" onClick={toggleSidebar}>
-            <IconMenu2 />
-          </Button>
-        ) : (
-          <NavbarMenu components={ProfessionalSection} />
-        )}
+        <NavbarMenu components={ProfessionalSection} />
       </div>
       <div className="ml-auto mr-2 flex">
         <ModeToggle />
