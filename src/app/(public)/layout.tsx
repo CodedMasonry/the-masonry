b2ff301d728966ef7db344c08e0302bd2eb30322
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2025-05-14 21:07:57 -0400

    remove unused import

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -5,33 +5,32 @@ import type { Metadata } from "next";
 import Footer from "~/components/footer";
 import { Navbar } from "~/components/navbar";
 import { ThemeProvider } from "~/components/theme-provider";
-import { ProfessionalSection } from "~/lib/routing";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-	title: "Brock Shaffer",
-	description: "A guy who made a website",
-	icons: [{ rel: "icon", url: "/favicon.svg" }],
+  title: "Brock Shaffer",
+  description: "A guy who made a website",
+  icons: [{ rel: "icon", url: "/favicon.svg" }],
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="light"
-			disableTransitionOnChange
-		>
-			<TRPCReactProvider>
-				<Navbar />
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      disableTransitionOnChange
+    >
+      <TRPCReactProvider>
+        <Navbar />
 
-				<main className="custombackground flex min-h-svh flex-col">
-					{children}
-				</main>
+        <main className="custombackground flex min-h-svh flex-col">
+          {children}
+        </main>
 
-				<Footer />
-			</TRPCReactProvider>
-		</ThemeProvider>
-	);
+        <Footer />
+      </TRPCReactProvider>
+    </ThemeProvider>
+  );
 }
