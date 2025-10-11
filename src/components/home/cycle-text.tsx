2200f4cf238817692a790b347913dcf75e72da95
CodedMasonry <109301662+CodedMasonry@users.noreply.github.com>
2025-10-11 17:56:45 -0400

    Add blur to rotating title

--- src/components/home/cycle-text.tsx
+++ src/components/home/cycle-text.tsx
@@ -4,13 +4,11 @@ import { useEffect, useState } from "react";
 export function CycleText({ options }: { options: Array<string> }) {
   const [index, setIndex] = useState(0);
 
-  // Rotate text automatically on an interval
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prevIndex) => (prevIndex + 1) % options.length);
     }, 3000);
-
-    return () => clearInterval(interval); // Cleanup interval on unmount
+    return () => clearInterval(interval);
   }, [options.length]);
 
   return (
@@ -18,9 +16,17 @@ export function CycleText({ options }: { options: Array<string> }) {
       <AnimatePresence mode="sync">
         <motion.div
           key={options[index]}
-          initial={{ opacity: 0, y: -25 }}
-          animate={{ opacity: [0, 0, 1], y: 0 }}
-          exit={{ opacity: [1, 0, 0], y: 25 }}
+          initial={{ opacity: 0, y: -25, filter: "blur(10px)" }}
+          animate={{
+            opacity: [0, 0, 1],
+            y: 0,
+            filter: ["blur(10px)", "blur(5px)", "blur(0px)"],
+          }}
+          exit={{
+            opacity: [1, 0, 0],
+            y: 25,
+            filter: ["blur(0px)", "blur(5px)", "blur(10px)"],
+          }}
           transition={{ duration: 0.5, times: [0, 0.5, 1] }}
           className="absolute top-0 left-0 underline decoration-primary"
         >
