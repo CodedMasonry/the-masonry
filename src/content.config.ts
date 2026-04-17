71c358646b3f306bd24b8edda46f871ba52563fe
Brock Shaffer <109301662+CodedMasonry@users.noreply.github.com>
2026-04-16 21:48:33 -0400

    update to latest and add prerender to static pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
 export const collections = {
@@ -7,30 +7,5 @@ export const collections = {
       folder: "TheMasonry",
       limit: 1000,
     }),
-    schema: z
-      .object({
-        id: z.string(),
-        data: z
-          .object({
-            asset_id: z.string(),
-            public_id: z.string(),
-            format: z.string(),
-            version: z.number(),
-            resource_type: z.enum(["image", "video", "raw"]),
-            type: z.enum(["upload", "private", "authenticated"]),
-            created_at: z.string().datetime(),
-            bytes: z.number(),
-            width: z.number(),
-            height: z.number(),
-            asset_folder: z.string(),
-            display_name: z.string(),
-            url: z.string().url(),
-            secure_url: z.string().url(),
-          })
-          .passthrough(),
-        digest: z.string(),
-        collection: z.string(),
-      })
-      .passthrough(),
   }),
 };
